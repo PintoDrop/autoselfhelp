f94db4438747aec009277023f22fd19ceed30b61
PintoDrop <mikebender22@gmail.com>
2023-04-03 14:05:56 -0700

    adding some starter code

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const apiRoutes = require("./api");
+const main = require('./main');
+
+router.use("/", main);
+router.use("/api", apiRoutes);
+
+module.exports = router;
