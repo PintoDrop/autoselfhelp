de2e89f92758db13bdf340a1fbb9a83540e94c96
PintoDrop <mikebender22@gmail.com>
2023-04-04 10:57:59 -0700

    trouble shooting server error

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {
+
+}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+  };
+);
+
+module.exports = Post;
