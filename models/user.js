6605c09df689b8ab710813582589d0e08ad1eec2
PintoDrop <mikebender22@gmail.com>
2023-04-05 13:21:43 -0700

    merging from main

--- models/user.js
+++ models/user.js
@@ -36,26 +36,25 @@ User.init(
       },
     },
   },
-  {
-    hooks: {
-      beforeCreate: async (newUserData) => {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-      beforeUpdate: async (updatedUserData) => {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
-        return updatedUserData;
-      },
-    },
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "user",
-  }
+  // {
+  //   hooks: {
+  //     beforeCreate: async (newUserData) => {
+  //       newUserData.password = await bcrypt.hash(newUserData.password, 10);
+  //       return newUserData;
+  //     },
+  //     beforeUpdate: async (updatedUserData) => {
+  //       updatedUserData.password = await bcrypt.hash(
+  //         updatedUserData.password,
+  //         10
+  //       );
+  //       return updatedUserData;
+  //     },
+  //   }
+      {
+    sequelize
+    
+     }
+  // }
 );
 
 
