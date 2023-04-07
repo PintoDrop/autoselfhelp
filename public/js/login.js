a9560ae88b7a991f20bd445487a6def9432248a8
dirtydenny <dallen7@socal.rr.com>
2023-04-07 15:24:29 -0700

    with bobbi

--- public/js/login.js
+++ public/js/login.js
@@ -1,9 +1,9 @@
-const loginFormHandler = async function (event) {
+const loginFormHandler = async function(event) {
   console.log("hi herehere");
   event.preventDefault();
   const emailEL = document.querySelector("#email");
   const passwordEl = document.querySelector("#password");
-  fetch("/api/user/login", {
+  fetch("/api/users/login", {
     method: "POST",
     body: JSON.stringify({
       email: emailEL.value,
