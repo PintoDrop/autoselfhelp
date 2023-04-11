38672be44d3144815240be123183cd59f953be59
PintoDrop <mikebender22@gmail.com>
2023-04-11 09:24:09 -0700

    adding back in some code

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const helpers = require("./utils/helpers");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const sequelize = require("./config/connection");
 // const generator = require("generate");
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
